5de030961466a6c58bb4f73c3e9f1a2006643051
Samuel <s.burtenshaw1@gmail.com>
2022-07-07 19:37:04 +1200

    Removed Git Merge Artifacts

--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 RESPONSE=$(curl -G -s -u https://api.github.com/repos/uccser/cs-field-guide/actions/runs -d "status=in_progress")
-=======
-RESPONSE=$(curl -G -s -u https://api.github.com/repos/uccser/cs-field-guide/actions/runs -d "per_page=8")
-
->>>>>>> 47ddd1b7c6923097e7458d9861da6aeabdfa6238
 
 # can use tag for prod or "develop" for development branch
 BRANCH="develop"
@@ -11,13 +6,9 @@ BRANCH="develop"
 # Can be optimised, if response is too big might fail
 IN_PROGRESS=$(jq -r -n --argjson data "${RESPONSE}" '$data.workflow_runs[] | select(.head_branch == "'"${BRANCH}"'" and .name == "Test and deploy") | any ')
 
-<<<<<<< HEAD
 if [ -z "${IN_PROGRESS}" ]; 
     then
         echo false
     else
         
 fi
-=======
-echo ${WORK_FLOW_RUNS}
->>>>>>> 47ddd1b7c6923097e7458d9861da6aeabdfa6238
